97a98d57483e47809df20a35e1b2b94a872ccfac
TESLA <tesla@robotcasserole.com>
2017-01-10 19:45:24 -0600

    copied lib from casserole lib 0.3.0

---
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/lib/Hourmeter/OperationState.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team1736.lib.Hourmeter;
+
+/**
+ * Describes what state the robot is in overall (disabled, teleop, auto, etc.). This is dictated by
+ * the
+ *
+ */
+public enum OperationState {
+    DISABLED, TELEOP, AUTO, TEST, UNKNOWN
+}
