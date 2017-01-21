8cd13dfef70a2689392bb0febd5f9219755c67c5
TESLA <tesla@robotcasserole.com>
2017-01-21 13:38:15 -0600

    Created shooter control class

--- EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/Sht_ctrl.java
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/Sht_ctrl.java
@@ -5,19 +5,27 @@ import org.usfirst.frc.team1736.lib.Calibration.Calibration;
 public class Sht_ctrl {
 	 ShooterWheelCTRL shooterControl;
 	 HopperControl hopControl;
-	 public Sht_ctrl(){
-	 Calibration wheel_Set_Point_rpm;	 
-			 
+	 public Sht_ctrl(){ 
+	 }
+	 Calibration wheel_Set_Point_rpm;
 		 
 	 public void update(){
-		 if((Shooter_States.SHOOT== RobotState.opShotCTRL) & RobotState.shooterVelocityOk){
-		 RobotState.hopperFeedCmd=true;
+		  if(Shooter_States.NO_Shoot==RobotState.opShotCTRL){
+		  RobotState.hopperFeedCmd=false;
+		  RobotState.shooterDesiredVelocity_rpm=0; 
+		  }
+		  else if(Shooter_States.PREP_TO_SHOOT==RobotState.opShotCTRL){
+		  RobotState.hopperFeedCmd=false;
+		  RobotState.shooterDesiredVelocity_rpm=10;
+		  }
+		  else if((Shooter_States.SHOOT== RobotState.opShotCTRL) & RobotState.shooterVelocityOk){
+		  RobotState.hopperFeedCmd=true;
+		  RobotState.shooterDesiredVelocity_rpm=10;
+		  else{
+			  RobotState.hopperFeedCmd=false;
+			  RobotState.shooterDesiredVelocity_rpm=10;  
+		  }
 		 }
-		 else{
-			 Shooter_States.NO_Shoot ;
-		{ 
-			if(){
-				
-			}
-		}
-	 }
+	}
+ }
+		 
