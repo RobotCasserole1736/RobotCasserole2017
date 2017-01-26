d131ded42455bf42a2625d964a59b2eb1e7bf0df
Chris Gerth <chrisgerth010592@gmail.com>
2017-01-25 22:02:30 -0600

    Integrated vision alignment outputs into shot control (inhibit a commanded shot when vision alignment is requested but not on target)

--- EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/Sht_ctrl.java
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/Sht_ctrl.java
@@ -14,18 +14,36 @@ public class Sht_ctrl {
 		 
 	 public void update(){
 		  if(Shooter_States.NO_Shoot==RobotState.opShotCTRL){
+			  //Operator requests everything turned off.
 			  RobotState.hopperFeedCmd=false;
 			  RobotState.shooterDesiredVelocity_rpm=0; 
 		  }
 		  else if(Shooter_States.PREP_TO_SHOOT==RobotState.opShotCTRL){
+			  //Operator wants to prepare to shoot. Today, this means spooling up the shooter wheel.
 			  RobotState.hopperFeedCmd=false;
 			  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();
 		  }
 		  else if((Shooter_States.SHOOT==RobotState.opShotCTRL) & RobotState.shooterVelocityOk){
-			  RobotState.hopperFeedCmd=true;
-			  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();
+			  //Operator wants to take the shot, and the shooter RPM is up to speed
+			  if(RobotState.visionAlignmentDesiried){
+				  //Driver has robot under automatic (vision-assist) alignment. 
+				  if(RobotState.visionAlignmentPossible & RobotState.isVisionAlignmentOnTarget()){
+					  //Vision alignment reports we are on target. Take the shot.
+					  RobotState.hopperFeedCmd=true;
+					  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();
+				  } else {
+					  //Inhibit shot until vision alignment is achieved
+					  RobotState.hopperFeedCmd=false;
+					  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();
+				  }
+			  } else {
+				  //Shooter is under manual alignment, just take the shot if RPM is ok
+				  RobotState.hopperFeedCmd=true;
+				  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();
+			  }
 		  }
-		  else{
+		  else{ //Shot desired but wheel RPM is not OK
+			  //Just spool the wheel back up. Hopefully we get it fast enough to take a shot soon.
 			  RobotState.hopperFeedCmd=false;
 			  RobotState.shooterDesiredVelocity_rpm=wheel_Set_Point_rpm.get();  
 		  }
