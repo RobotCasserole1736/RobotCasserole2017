810cf2beaf2f4292dcbded6f23df8af4f98ede6a
Chris Gerth <chrisgerth010592@gmail.com>
2017-02-01 18:58:35 -0600

    Fixed user libs deploy script to not care about rio changing mac address (which happens)

--- EclipseProject/RobotCasserole2017/deployUserLibs.sh
+++ EclipseProject/RobotCasserole2017/deployUserLibs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "If prompted for password, just hit enter (it's blank)"
-scp lib/native/lib/*.so admin@roborio-1736-frc.local:/usr/local/frc/lib/
-pause
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no lib/native/lib/*.so admin@roborio-1736-frc.local:/usr/local/frc/lib/
+read -p "Press enter to exit..."
