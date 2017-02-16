0ca0a04737a4c139c4b4e5c5191bc26fab434222
Chris Gerth <chrisgerth010592@gmail.com>
2017-02-15 22:13:38 -0600

    Fix solenoid init implementation error

--- EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/IntakeControl.java
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/IntakeControl.java
@@ -35,7 +35,7 @@ public class IntakeControl {
 	
 	//Declare Extender Control
 	Solenoid intakeHPExtend = new Solenoid(RobotConstants.INTAKE_HP_EXTEND_SOLENOID_PORT);
-	Solenoid intakeLPExtend = new Solenoid(RobotConstants.INTAKE_HP_EXTEND_SOLENOID_PORT);
+	Solenoid intakeLPExtend = new Solenoid(RobotConstants.INTAKE_LP_EXTEND_SOLENOID_PORT);
 	
 	//Declaring Intake Calibration
 	Calibration intakeMotorFwdCmd = new Calibration("Ground Pickup Intake Motor Command", 0.5, 0.0, 1.0);
