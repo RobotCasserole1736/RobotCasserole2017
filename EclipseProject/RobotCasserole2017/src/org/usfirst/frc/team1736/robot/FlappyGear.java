c0fe936889621b5a6749ddf6477d3ea141fe3ed6
Chris Gerth <chrisgerth010592@gmail.com>
2017-02-20 16:39:05 -0600

    tweaked flap position

--- EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/FlappyGear.java
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/FlappyGear.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Servo;
 public class FlappyGear {
 
 	int FLAP_UP_DEG = 190;
-	int FLAP_DOWN_DEG = 45;
+	int FLAP_DOWN_DEG = 65;
 	Servo servo;
 	public FlappyGear(){
 	 servo = new Servo(RobotConstants.GEAR_FLAP_SERVO_PWM_PORT);	
