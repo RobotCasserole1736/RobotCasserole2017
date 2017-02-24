18d7824cbd564f9e4c90f6c874189e09b19994ea
Chris Gerth <chrisgerth010592@gmail.com>
2017-02-23 20:00:34 -0600

    Updated calculation algorithm
    Change from using RPM to current, look for sharp negative dips in second deriv. of current.
    updated threshold

--- Calculators/shot_jerk_thresh_eval.m
+++ Calculators/shot_jerk_thresh_eval.m
@@ -1,24 +1,43 @@
-fpath = ".\\log_2016-07-30_122429.csv";
+fpath = ".\\log_21-Feb-2017_07.47.06PM_Teleop.csv";
 data = csvread(fpath);
 time = data(3:end,1);
-shooterRPM = data(3:end,53);
+shooterCurrent = data(3:end,16);
 
 dt = diff(time);
 
-RPMps = diff(shooterRPM)./dt;
+%shooterCurrentFilt = zeros(length(shooterCurrent),1);
+%shooterCurrentFilt(1) = shooterCurrent(1);
+
+%for idx = 2:length(shooterCurrent)
+%  shooterCurrentFilt(idx) = (shooterCurrent(idx) + shooterCurrent(idx-1))/2;
+%end
+
+Aps = (diff(shooterCurrent)./dt);
 
 
-RPMpsps = diff(RPMps)./dt(1:end-1);
+Apsps = diff(Aps)./dt(1:end-1);
 
 
-RPMpspsFilt = zeros(length(RPMpsps),1);
+ApspsFilt = zeros(length(Apsps),1);
 
-RPMpspsFilt(1) = RPMpsps(1);
-RPMpspsFilt(2) = RPMpsps(2);
-for idx = 3:length(RPMpsps)
-  RPMpspsFilt(idx) = (RPMpsps(idx) + RPMpsps(idx-1) + RPMpsps(idx-2))/3;
+ApspsFilt(1) = Apsps(1);
+ApspsFilt(2) = Apsps(2);
+ApspsFilt(3) = Apsps(3);
+ApspsFilt(4) = Apsps(4);
+
+for idx = 5:length(Apsps)
+  ApspsFilt(idx) = (Apsps(idx) + Apsps(idx-1) + Apsps(idx-2) + Apsps(idx-3) + Apsps(idx-4))/5;
+end
+
+shotCount = zeros(length(ApspsFilt),1);
+for idx = 2:length(shotCount)
+  if((ApspsFilt(idx) < -4000) && (ApspsFilt(idx-1) >= -4000))
+    shotCount(idx) = shotCount(idx-1)+1;
+  else
+    shotCount(idx) =shotCount(idx-1);
+  end
 end
 
 
-
-plot(time(1:end-2), shooterRPM(1:end-2), time(1:end-2), RPMpspsFilt);
+%plot(time(1:end-2), shooterCurrent(1:end-2), time(1:end-2), ApspsFilt./100);
+plot(time(1:end-2), shooterCurrent(1:end-2), time(1:end-2), shotCount);
