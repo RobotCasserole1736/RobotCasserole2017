b74e0cfd94efba7ba1fd7870604ba798f356d4a2
Chris Gerth <chrisgerth010592@gmail.com>
2017-03-21 20:15:14 -0500

    tweaked setpoints based on some random guessing.

--- EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/auto/AutoEventCatchHopper.java
+++ EclipseProject/RobotCasserole2017/src/org/usfirst/frc/team1736/robot/auto/AutoEventCatchHopper.java
@@ -11,7 +11,7 @@ public class AutoEventCatchHopper extends AutoEvent{
 	
 	private static final double[][] waypoints = new double[][]{
 		{0,0,0},
-		{0,-2,0}
+		{0,-3,0}
 	};
 	
 	private static final double time = 1;
